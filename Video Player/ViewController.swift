8f6e623a815d5c6844054cec196689564b4e8f45
Owais Quadri <owaisquadri01@gmail.com>
2023-07-08 13:00:02 -0400

    Initial Commit

---
+++ Video Player/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Video Player
+//
+//  Created by Owais on 2023-07-08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
